7f9be298723c3c55fdb18290db4d7ae71ccdf510
Yelyzaveta Shevchenko <elizabeth.shevchenko@gmail.com>
2022-11-29 12:22:08 +0100

    decisions controller

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -1,2 +1,33 @@
 class DecisionsController < ApplicationController
+  # skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @decisions = Decision.all
+  end
+
+  def show
+    @decision = Decision.find(params[:id])
+  end
+
+  def new
+    @decision = Decision.new
+  end
+
+  def create
+    @decision = Decision.new(decision_params)
+    @decision.save
+    redirect_to option_path
+  end
+
+  def destroy
+    @decision = Decision.find(params[:id])
+    @decision.destroy
+    redirect_to decision_path, status: :see_other
+  end
+
+  private 
+
+  def decision_params
+    params.require(:decision).permit(:decision_name)
+  end
 end
