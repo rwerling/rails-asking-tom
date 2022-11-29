db7d0ab2ef9722bbc0f7026d657d59e9b3e64fe2
Sabrina Ahamed <tazrian.zawad14@gmail.com>
2022-11-29 15:08:57 +0100

    options

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,3 +1,4 @@
 class Decision < ApplicationRecord
   belongs_to :user
+  has_many :options, dependent: :destroy
 end
--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,4 @@
 class Option < ApplicationRecord
   belongs_to :decision
+  has_many :arguments, dependent: :destroy
 end
