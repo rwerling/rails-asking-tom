ad271bd5dfa4b6d375275d80716ff441c3baa30a
Xiomara Guillorme <xi.guillorme@gmail.com>
2022-12-07 13:25:32 +0100

    meta-tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 end
+
+def default_url_options
+  { host: ENV["DOMAIN=www.asktom.me"] || "localhost:3000" }
+end
